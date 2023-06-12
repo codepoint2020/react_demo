bb45ed3a6149b1075bfd61fa46d0742fdc428a69
j-morales-code <jeromemorales86@live.com>
2023-06-12 22:35:34 +0800

    Link added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorite";
+import MainNavigation from "./components/layout/MainNavigation";
+
+
 function App() {
-  return <div></div>;
+  return <div>
+    <MainNavigation />
+    <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <App />
- 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>   
 );
 
 
