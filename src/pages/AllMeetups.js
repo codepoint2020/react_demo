a0aaee86b74c8bd3a33dcd613beab61ecc78b793
j-morales-code <jeromemorales86@live.com>
2023-06-13 22:24:24 +0800

    Pages, Links plus Rendering Data using Map filter

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,35 @@
 function AllMeetupsPage() {
-    return <div>All Meetups Page</div>;
+
+    const DUMMY_DATA = [
+        {
+          id: 'm1',
+          title: 'This is a first meetup',
+          image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+          address: 'Meetupstreet 5, 12345 Meetup City',
+          description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+        },
+        {
+          id: 'm2',
+          title: 'This is a second meetup',
+          image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+          address: 'Meetupstreet 5, 12345 Meetup City',
+          description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+        }
+      ];
+
+
+    return <div>
+            <h1>All Meetups Page </h1>
+            {DUMMY_DATA.map((meetup, index) => (
+                <ul key={index}>
+                    <li> <h2>{meetup.title}</h2> <p>{meetup.description}</p>  </li>
+                </ul>
+            ))}
+        </div>;
 }
 
 export default AllMeetupsPage;
